b3f6ccdf7af99c1f7ebfce3f5e40e9f133d58fd9
Kouhei Tsuru <kktturu1993@gmail.com>
2024-01-22 23:22:38 +0900

    現状確認step4-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password_digest, presence: true, length: { minimum: 6 }, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false, message: 'はすでに使用されています' },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: 'の形式が不正です' }
+  validates :password, length: { minimum: 6, message: 'は6文字以上で入力してください' }
+
   before_validation { email.downcase! }
-  validates :password, presence: true, length: { minimum: 6 }
+
   has_many :tasks, dependent: :destroy
 end
