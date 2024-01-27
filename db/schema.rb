1060652b61fa83304dd4671690912048f4b1c5b7
Kouhei Tsuru <kktturu1993@gmail.com>
2024-01-27 09:22:26 +0900

    user_idに対してインデックスを貼る

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_22_125133) do
+ActiveRecord::Schema.define(version: 2024_01_27_001520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2024_01_22_125133) do
     t.integer "status", null: false
     t.integer "user_id"
     t.index ["status"], name: "index_tasks_on_status"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
